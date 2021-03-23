4843ccec26d98322e539d56a227a03d93b03ac39
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-03-23 16:02:25 +0000

    HOCS-2527: fix CLUSTER_NAME assignment
    Currently we have not specified the `CLUSTER_NAME` so will fail on 
    deploy. This change moves it below and assigns the `CLUSTER_NAME`
    a value.

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,22 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
-export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
-
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
     export MIN_REPLICAS="2"
     export MAX_REPLICAS="6"
+
+    export CLUSTER_NAME="acp-prod"
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="3"
+
+    export CLUSTER_NAME="acp-notprod"
 fi
 
-cd kd
+export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
+
+cd kd || exit 1
 
 kd --timeout 15m \
     -f deployment.yaml \
